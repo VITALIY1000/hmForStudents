32e6c97bacb8886a394ab64a94b3b851457f4791
VITALIY1000 <vitalytyrenko1000@gmail.com>
2019-02-07 01:02:22 +0200

    Finished

--- README.md
+++
@@ -1,2 +0,0 @@
-# hmForStudents
-https://gist.github.com/polyak01/740a97dd73f60cd794eec3d56411dc2a
--- src/ua/com/samsungitschool/IListDeleteByFilter.java
+++
@@ -1,7 +0,0 @@
-package ua.com.samsungitschool;
-
-import java.util.List;
-
-public interface IListDeleteByFilter<T> {
-    List<T> deleteFromList(List<T> list, T deleteFilterKey);
-}
