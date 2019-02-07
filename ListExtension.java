b7270734bebeaebc1957bfeec895715ffc53f274
VITALIY1000 <vitalytyrenko1000@gmail.com>
2019-02-07 20:22:56 +0200

    ListExtention.java updated

--- ListExtension.java
+++ ListExtension.java
@@ -6,6 +6,11 @@ public class ListExtension implements IListDeleteByFilter<Integer>{
 
     @Override
     public List<Integer> deleteFromList(List<Integer> list, Integer deleteFilterKey) {
-        return null;
+    	
+        while (list.indexOf(deleteFilterKey) >= 0) {
+          list.remove(list.indexOf(deleteFilterKey));
+        }
+        
+        return list;
     }
 }
